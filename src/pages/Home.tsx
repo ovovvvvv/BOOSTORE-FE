0687a77496790b26f5f471e099d6ab458e3e9a40
ovovvvvv <ovovvvvv@naver.com>
2024-03-06 14:51:54 +0900

    추가구현: 메인에 신간, 리뷰 노출

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,20 +3,29 @@ import styled from "styled-components";
 import Title from "@/components/common/Title";
 import MainReview from "@/components/main/MainReview";
 import { useMain } from "@/hooks/useMain";
+import MainNewBooks from "@/components/main/MainNewBooks";
 
 const Home = () => {
-  const { reviews } = useMain();
+  const { reviews, newBooks } = useMain();
   useEffect(() => {}, []);
   return (
     <HomeStyle>
       {/* 배너 */}
 
       {/* 베스트 셀러 */}
-
+      <section className="section">
+        <Title size="large">베스트 셀러</Title>
+      </section>
       {/* 신간 */}
-
+      <section className="section">
+        <Title size="large">신간 안내</Title>
+        <MainNewBooks books={newBooks} />
+      </section>
       {/* 리뷰 */}
-      <MainReview reviews={reviews} />
+      <section className="section">
+        <Title size="large">리뷰</Title>
+        <MainReview reviews={reviews} />
+      </section>
     </HomeStyle>
   );
 };
